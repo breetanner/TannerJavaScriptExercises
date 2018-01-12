2d3e6385eb3f1a369694afe043fa2dca4ebeba32
breetanner <bree@studio-us.com>
2018-01-11 21:56:24 -0500

    initial project version

---
+++ script.js
@@ -0,0 +1,47 @@
+// ----- EXCERCISE 1 -----------------
+var num = prompt("Enter a Number");
+
+var total = 0;
+for (var x = 1; x<=num; x++) {
+  total += x;
+}
+
+console.log(total)
+
+// ----- EXCERCISE 2 ----------------
+
+var play = prompt("do you want to play?")
+  if (play == "yes" ) {
+   do { var word = prompt ("Enter a Word");
+        document.write(word);
+        var val = prompt ("do you want to play again?");
+   } while (val == "yes")
+  }
+  console.log()
+
+  // ----- EXCERCISE 3 -----------------
+
+  var answer = prompt("Would you like to print your name?");
+  var name = "Hello. My name is Adam";
+  var n = "!"
+
+  if (answer == "yes" ) {
+  do {
+    name = name + n
+    console.log(name)
+        var val = prompt ("Would you like to print this again?");
+    } while (val == "yes")
+  }
+
+  // ----- EXCERCISE 4 -----------------
+
+ var input = prompt("Please enter a time of day (mornining, noon, or evening).");
+   if (input == "morning") {
+      console.log("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
+     }else if(input == "evening") {
+      console.log("Since it is evening, you should be eating dinner. We suggest chicken and rice.")
+     }else if(input == "noon"){
+       console.log("Since it is noon, you should be eating lunch. We suggest a salad.")
+     }else{
+       console.log("Not a valid entry, please enter either morning, noon, or evening")
+     }
